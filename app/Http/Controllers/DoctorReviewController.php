afef9274d51b71eee3dc64ded986225b347ddfd5
Zurthr <outdatin@gmail.com>
2025-05-29 05:27:05 +0700

    SI46INT3-49 Fixed mayla's review work, pushing the doctor review WITHOUT authentication for Review-Doctor

--- app/Http/Controllers/DoctorReviewController.php
+++ app/Http/Controllers/DoctorReviewController.php
@@ -9,14 +9,24 @@ use Illuminate\Support\Facades\Auth;
 
 class DoctorReviewController extends Controller
 {
-    public function index()
-    {
-        $doctorId = Auth::guard('doctor')->id();
+    // public function index()
+    // {
+    //     $doctorId = Auth::guard('doctor')->id();
 
-        // Fetch only 'appointment' reviews that are sent and belong to this doctor
+    //     // Fetch only 'appointment' reviews that are sent and belong to this doctor
+    //     $reviews = Review::where('category', 'appointment')
+    //                      ->where('status', 1)
+    //                      ->where('doctor_id', $doctorId)
+    //                      ->orderBy('submitted_at', 'desc')
+    //                      ->get();
+
+    //     return view('doctordash.reviews.index', compact('reviews'));
+    // }
+        public function index()
+    {
+
         $reviews = Review::where('category', 'appointment')
                          ->where('status', 1)
-                         ->where('doctor_id', $doctorId)
                          ->orderBy('submitted_at', 'desc')
                          ->get();
 
